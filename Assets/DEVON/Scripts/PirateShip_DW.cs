ce0f2e79f6108689844d84d1c45dced850a8a16a
dwilliams73 <devongrad2022@gmail.com>
2026-02-25 17:13:21 -0600

    Pirate Animation Progress - State Change

--- Assets/DEVON/Scripts/PirateShip_DW.cs
+++ Assets/DEVON/Scripts/PirateShip_DW.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class PirateShip_DW : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Animator References")]
+    public Animator orbitAnimator;   // Animator on the empty orbit object
+    public Animator shipAnimator;    // Animator on the spaceship
 
-    // Update is called once per frame
     void Update()
     {
-        
-        //if Input.GetKeyDown(KeyCode.T){
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("T is pressed");
 
+            // Stop the orbit animation (return to origin)
+            if (orbitAnimator != null)
+                orbitAnimator.SetTrigger("StopOrbit");
 
-
-       // }
-
-
+            // Start attack animation on the ship
+            if (shipAnimator != null)
+                shipAnimator.SetTrigger("Attack");
+        }
     }
 }
