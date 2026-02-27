9891fd2271add8107b004d828e9f66a52f67765c
marleyk02 <58349504@nebraska.edu>
2026-02-27 13:47:51 -0600

    HealthBarWorking
    Just edited a script and its WORKING YAY!!

--- Assets/MARLEY/Scripts_MS/HealthBarLower_MS.cs
+++ Assets/MARLEY/Scripts_MS/HealthBarLower_MS.cs
@@ -5,33 +5,47 @@ using UnityEngine;
 public class HealthBarLower_MS : MonoBehaviour
 {
     public int ShieldHealth_MS;
+    public float YScale_MS;
+    public GameObject PivotPoint;
 
     void Start()
     {
         ShieldHealth_MS = 100;
+        YScale_MS = 1.0f;
         StartCoroutine(LowerVariable());
-    }
-
-    public IEnumerator LowerVariable()
-    {
-        while(ShieldHealth_MS >= 5)
-            {
-                ShieldHealth_MS -= 5;
-                yield return new WaitForSeconds(5);
-                Debug.Log(ShieldHealth_MS);
-            }
-        //return LowerVariable();
+        PivotPoint = GameObject.Find("PivotPoint");
     }
 
     void Update()
     {
-
-
-
-        if(ShieldHealth_MS <= 95 && Input.GetKeyDown(KeyCode.P))
+        YScale_MS = ShieldHealth_MS / 100f;
+        PivotPoint.transform.localScale = new Vector3(1f, YScale_MS, 1f);
+        if (ShieldHealth_MS <= 0)
         {
-            ShieldHealth_MS += 5;
+            ShieldHealth_MS = 0;
+            Debug.Log("Shield gone, you're gonna freaking die");
         }
 
+        if(ShieldHealth_MS <= 95 && Input.GetKeyDown(KeyCode.F))
+        {
+            ShieldHealth_MS = 100;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ShieldHealth_MS = ShieldHealth_MS / 2 - ShieldHealth_MS % 1;
+        }
+
+        
+    }
+
+    public IEnumerator LowerVariable()
+    {
+        while (ShieldHealth_MS >= 5)
+        {
+            ShieldHealth_MS -= 5;
+            yield return new WaitForSeconds(5);
+            Debug.Log(ShieldHealth_MS);
+        }
     }
 }
