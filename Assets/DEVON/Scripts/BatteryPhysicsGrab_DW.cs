d09cee3a7110285701fd444b33668eae8fdf0ba8
dwilliams73 <devongrad2022@gmail.com>
2026-03-30 16:51:34 -0500

    Battery Trash Chute Door Animation

---
+++ Assets/DEVON/Scripts/BatteryPhysicsGrab_DW.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using System.Collections;
+
+[RequireComponent(typeof(Interactable))]
+[RequireComponent(typeof(Rigidbody))]
+public class BatteryPhysicsGrab_DW : MonoBehaviour
+{
+    public BatterySnapZone snapZone; // assign in inspector
+    public float followStrength = 50f; // tweak for hand responsiveness
+
+    private Rigidbody rb;
+    private Hand attachedHand;
+    private Interactable interactable;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        interactable = GetComponent<Interactable>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Physics-based following
+        if (attachedHand != null)
+        {
+            Vector3 targetPos = attachedHand.transform.position;
+            Vector3 move = targetPos - rb.position;
+            rb.velocity = move * followStrength * Time.fixedDeltaTime;
+
+            // Optional: match rotation
+            Quaternion targetRot = attachedHand.transform.rotation;
+            Quaternion deltaRot = targetRot * Quaternion.Inverse(rb.rotation);
+            deltaRot.ToAngleAxis(out float angle, out Vector3 axis);
+            if (angle > 180) angle -= 360;
+            rb.angularVelocity = axis * angle * Mathf.Deg2Rad * followStrength * Time.fixedDeltaTime;
+        }
+    }
+
+    // Called by SteamVR when hand grabs object
+    private void OnAttachedToHand(Hand hand)
+    {
+        attachedHand = hand;
+
+        rb.isKinematic = false;
+        rb.useGravity = false; // optional, keep it controlled by hand
+    }
+
+    // Called by SteamVR when hand releases object
+    private void OnDetachedFromHand(Hand hand)
+    {
+        attachedHand = null;
+
+        if (snapZone != null && snapZone.IsBatteryInZone(gameObject))
+        {
+            snapZone.TrySnap();
+        }
+        else
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+        }
+    }
+}
