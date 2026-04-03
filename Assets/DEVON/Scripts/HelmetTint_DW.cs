fb73ec493ff27e958b37793fc64f95d6f92945c7
dwilliams73 <devongrad2022@gmail.com>
2026-04-03 16:49:39 -0500

    Started Helmet Tinting

---
+++ Assets/DEVON/Scripts/HelmetTint_DW.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelmetTint_DW : MonoBehaviour
+{
+	//public Animator TrashChuteAnimator;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("TrashBattery"))
+		{
+			//if (TrashChuteAnimator != null)
+			//	TrashChuteAnimator.SetTrigger("ChuteClose");
+
+			Debug.Log("Trash Chute door closing");
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("TrashBattery"))
+		{
+		//	if (TrashChuteAnimator != null)
+			//	TrashChuteAnimator.SetTrigger("ChuteOpen");
+
+			Debug.Log("Trash Chute door opening");
+		}
+	}
+
+}
