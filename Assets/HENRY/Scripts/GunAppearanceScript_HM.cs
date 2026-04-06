cb5422649463094d191033acbe5906e1ef2d591d
Henry Monahan <179609706+HernyHank@users.noreply.github.com>
2026-04-06 16:15:18 -0500

    FIXIN SHIPSIN

--- Assets/HENRY/Scripts/GunAppearanceScript_HM.cs
+++ Assets/HENRY/Scripts/GunAppearanceScript_HM.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class GunAppearanceScript_HM : MonoBehaviour
 {
     private Renderer gunRenderer;
+    EventController controller;
 
     void Start()
     {
+        controller = GameObject.Find("EmptyEventController").GetComponent<EventController>();
+        if(controller != null)
+        {
+            Debug.Log("found" + controller.name);
+        } else
+        {
+            Debug.Log("did NOT find controller");
+        }
         gunRenderer = GetComponent<Renderer>();
         gunRenderer.enabled = false; // hide at start
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        Debug.Log("Turret can shoot value: " + controller.turretCanShoot);
+        if (Input.GetKeyDown(KeyCode.K) && controller.turretCanShoot)
         {
             gunRenderer.enabled = true;
            // Debug.Log("gun is shot");
