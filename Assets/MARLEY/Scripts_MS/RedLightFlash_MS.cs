b43b5d88f881690c0a4112aa7345b5a109cb2d3e
marleyk02 <58349504@nebraska.edu>
2026-04-08 16:58:43 -0500

    RedLightsAndControlPanelUI
    Bofa are done.

---
+++ Assets/MARLEY/Scripts_MS/RedLightFlash_MS.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedLightFlash_MS : MonoBehaviour
+{
+    public Light pointLight;
+
+    public KeyCode toggleKey = KeyCode.F;
+
+    public float flashSpeed = 0.5f; // time between on/off
+
+    private bool isFlashing = false;
+    private Coroutine flashCoroutine;
+
+    void Start()
+    {
+        if (pointLight == null)
+        {
+            pointLight = GetComponent<Light>();
+        }
+
+        SetWhiteSolid();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(toggleKey))
+        {
+            ToggleLightMode();
+        }
+    }
+
+    void ToggleLightMode()
+    {
+        isFlashing = !isFlashing;
+
+        if (isFlashing)
+        {
+            SetRedFlashing();
+        }
+        else
+        {
+            SetWhiteSolid();
+        }
+    }
+
+    void SetWhiteSolid()
+    {
+        if (flashCoroutine != null)
+        {
+            StopCoroutine(flashCoroutine);
+        }
+
+        pointLight.color = Color.white;
+        pointLight.enabled = true;
+    }
+
+    void SetRedFlashing()
+    {
+        pointLight.color = Color.red;
+
+        if (flashCoroutine != null)
+        {
+            StopCoroutine(flashCoroutine);
+        }
+
+        flashCoroutine = StartCoroutine(FlashRoutine());
+    }
+
+    IEnumerator FlashRoutine()
+    {
+        while (true)
+        {
+            pointLight.enabled = !pointLight.enabled;
+            yield return new WaitForSeconds(flashSpeed);
+        }
+    }
+}
