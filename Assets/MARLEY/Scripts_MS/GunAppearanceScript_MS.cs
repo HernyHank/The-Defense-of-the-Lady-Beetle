e717ddae43d5622a5cf08d5cc745a6616b112ea5
marleyk02 <58349504@nebraska.edu>
2026-04-17 17:44:59 -0500

    MapTableFinished

--- Assets/MARLEY/Scripts_MS/GunAppearanceScript_MS.cs
+++ Assets/MARLEY/Scripts_MS/GunAppearanceScript_MS.cs
@@ -1,41 +1,10 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GunAppearanceScript_MS : MonoBehaviour
-{
-    private Renderer gunRenderer;
-
-    void Start()
-    {
-        gunRenderer = GetComponent<Renderer>();
-        gunRenderer.enabled = false; // hide at start
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            gunRenderer.enabled = true;
-            //play growing animation
-            Debug.Log("gun is shot");
-        }
-
-        if (Input.GetKeyUp(KeyCode.K))
-        {
-            gunRenderer.enabled = false;
-            //play shrinking animation
-        }
-    }
-}*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunAppearanceScript_MS : MonoBehaviour
 {
-    public float scaleTime = 0.01f; // time to fully grow/shrink
+    public float scaleTime = 0.005f; // time to fully grow/shrink
 
     private Vector3 targetScale;
     private float fixedY;
@@ -59,23 +28,28 @@ public class GunAppearanceScript_MS : MonoBehaviour
             isGrowing = true;
         }
 
-        if (Input.GetKeyUp(KeyCode.K))
-        {
-            isGrowing = false;
-        }
+
 
         Vector3 current = transform.localScale;
-        Vector3 target = isGrowing
-            ? targetScale
-            : new Vector3(0f, fixedY, 0f);
+
+        Vector3 shrinkTarget = new Vector3(0f, fixedY, 0f);
+
 
         // Calculate speed so it completes in exactly scaleTime
         float speed = 1f / scaleTime;
 
         // Move only X and Z, keep Y fixed
-        float newX = Mathf.MoveTowards(current.x, target.x, speed * Time.deltaTime);
-        float newZ = Mathf.MoveTowards(current.z, target.z, speed * Time.deltaTime);
+
+        float newX = Mathf.MoveTowards(current.x, isGrowing ? targetScale.x : shrinkTarget.x, speed * Time.deltaTime);
+        float newZ = Mathf.MoveTowards(current.z, isGrowing ? targetScale.z : shrinkTarget.z, speed * Time.deltaTime);
 
         transform.localScale = new Vector3(newX, fixedY, newZ);
+
+        if (isGrowing &&
+            Mathf.Approximately(newX, targetScale.x) &&
+            Mathf.Approximately(newZ, targetScale.z))
+        {
+            isGrowing = false;
+        }
     }
 }
