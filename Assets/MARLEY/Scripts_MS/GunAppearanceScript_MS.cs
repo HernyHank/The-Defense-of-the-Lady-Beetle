a173e153a579e7d262600cd1ea73a9ff439d77c3
marleyk02 <58349504@nebraska.edu>
2026-04-22 17:57:24 -0500

    ParticlesWorkingTurretBrokenAhh

--- Assets/MARLEY/Scripts_MS/GunAppearanceScript_MS.cs
+++ Assets/MARLEY/Scripts_MS/GunAppearanceScript_MS.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class GunAppearanceScript_MS : MonoBehaviour
 {
-    public float scaleTime = 0.005f; // time to fully grow/shrink
+    public float scaleTime = 1f; // time to fully grow/shrink
 
     private Vector3 targetScale;
     private float fixedY;
     private bool isGrowing = false;
+    private bool isShrinking = false;
+
+    public ParticleSystem particles; //assign in inspector
+
 
     void Start()
     {
@@ -23,24 +28,58 @@ public class GunAppearanceScript_MS : MonoBehaviour
 
     void Update()
     {
+        Debug.Log("UPDATE RUNNING - scale: " + transform.localScale);
         if (Input.GetKeyDown(KeyCode.K))
         {
             isGrowing = true;
+            isShrinking = false;
+            if (particles != null)
+            {
+                particles.Play();
+            }
+            //StartCoroutine(DisableAfterParticles());
+
         }
 
-
-
         Vector3 current = transform.localScale;
-
         Vector3 shrinkTarget = new Vector3(0f, fixedY, 0f);
 
 
         // Calculate speed so it completes in exactly scaleTime
         float speed = 1f / scaleTime;
 
-        // Move only X and Z, keep Y fixed
+        float newX = current.x;
+        float newZ = current.z;
 
-        float newX = Mathf.MoveTowards(current.x, isGrowing ? targetScale.x : shrinkTarget.x, speed * Time.deltaTime);
+        if (isGrowing)
+        {
+            newX = Mathf.MoveTowards(current.x, targetScale.x, speed * Time.deltaTime);
+            newZ = Mathf.MoveTowards(current.z, targetScale.z, speed * Time.deltaTime);
+        }
+        else if (isShrinking)
+        {
+            newX = Mathf.MoveTowards(current.x, shrinkTarget.x, speed * Time.deltaTime);
+            newZ = Mathf.MoveTowards(current.z, shrinkTarget.z, speed * Time.deltaTime);
+        }
+
+        transform.localScale = new Vector3(newX, fixedY, newZ);
+
+        if (isGrowing &&
+             Mathf.Approximately(newX, targetScale.x) &&
+             Mathf.Approximately(newZ, targetScale.z))
+        {
+            isGrowing = false;
+            isShrinking = true;
+        }
+
+        if (isShrinking &&
+            Mathf.Approximately(newX, shrinkTarget.x) &&
+            Mathf.Approximately(newZ, shrinkTarget.z))
+        {
+            isShrinking = false;
+        }
+
+        /*float newX = Mathf.MoveTowards(current.x, isGrowing ? targetScale.x : shrinkTarget.x, speed * Time.deltaTime);
         float newZ = Mathf.MoveTowards(current.z, isGrowing ? targetScale.z : shrinkTarget.z, speed * Time.deltaTime);
 
         transform.localScale = new Vector3(newX, fixedY, newZ);
@@ -50,6 +89,27 @@ public class GunAppearanceScript_MS : MonoBehaviour
             Mathf.Approximately(newZ, targetScale.z))
         {
             isGrowing = false;
+            isShrinking = true;
         }
+
+        if (isShrinking &&
+            Mathf.Approximately(newX, shrinkTarget.x) &&
+            Mathf.Approximately(newZ, shrinkTarget.z))
+        {
+            isShrinking = false;
+        }*/
     }
+    /*IEnumerator DisableAfterParticles()
+    {
+        if (particles != null)
+        {
+            yield return new WaitForSeconds(particles.main.duration);
+        }
+        else
+        {
+            yield return null;
+        }
+
+        this.gameObject.SetActive(false);
+    }*/
 }
